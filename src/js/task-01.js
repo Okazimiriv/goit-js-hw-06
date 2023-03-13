ed071ab01ba3999f685882686fa1f2296da15380
Oksana Komarnytska <okazimiriv@gmail.com>
2023-03-13 23:44:04 +0100

    task-01

---
+++ src/js/task-01.js
@@ -0,0 +1,15 @@
+const listItems = document.querySelectorAll('.item');
+console.log('Number of categories: ', listItems.length);
+
+//_______________
+
+
+listItems.forEach(function (number, index) {
+  
+const listTitle = listItems[index].querySelector('h2');
+  console.log('Category: ', listTitle.textContent); 
+  
+const listElem = listTitle.nextElementSibling.querySelectorAll('li');
+  console.log('Elements: ', listElem.length);
+});
+
