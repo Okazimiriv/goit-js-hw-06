18be3581c9dd05efe735195490fec536b53e313f
Oksana Komarnytska <okazimiriv@gmail.com>
2023-03-14 13:45:37 +0100

    task-02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -7,9 +7,9 @@ console.log('Number of categories: ', listItems.length);
 listItems.forEach(function (number, index) {
   
 const listTitle = listItems[index].querySelector('h2');
-  console.log('Category: ', listTitle.textContent); 
+console.log('Category: ', listTitle.textContent); 
   
 const listElem = listTitle.nextElementSibling.querySelectorAll('li');
-  console.log('Elements: ', listElem.length);
+console.log('Elements: ', listElem.length);
 });
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,38 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const makeIngredients = options => {
+  return options.map(option => {
+    const ingredientsEl = document.createElement('li');
+    ingredientsEl.textContent = option;
+    ingredientsEl.classList.add('item');
+
+    return ingredientsEl;
+  });
+};
+
+const elements = makeIngredients(ingredients);
+ingredientsList.append(...elements);
+
+//___________________
+// const ingredientsList = document.querySelector('#ingredients');
+// console.log(ingredientsList);
+   
+// const list = [];
+
+// for (let i = 0; i < ingredients.length; i += 1){
+//   const options = ingredients[i];
+
+//   const ingredientsElement = document.createElement('li');
+//   ingredientsElement.textContent = ingredients[i];
+//   ingredientsElement.classList.add('item');
+   
+//   list.push(ingredientsElement);
+//    console.log(ingredientsElement); 
+// };
+
+// ingredientsList.append(...list);
+// console.log(ingredientsList);
