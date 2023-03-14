3d7980f33b7cc9cc5ff7401cd99ac67725948201
Oksana Komarnytska <okazimiriv@gmail.com>
2023-03-15 00:13:22 +0100

    task-3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,45 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imagesList = document.querySelector('.gallery');
+imagesList.style.display = 'flex';
+imagesList.style.justifyContent = 'space-between';
+
+ const makeImageCard = ({ url, alt }) => {
+ imagesList.insertAdjacentHTML('beforeend', `<li> <img 
+      src="${url}" 
+       alt="${alt}" height = 1000>      
+</li>`);  
+  
+  return;
+};
+
+const elements = images.map(makeImageCard);
+
+//___________________________
+
+// const imagesList = document.querySelector('.gallery');
+// console.log(imagesList);
+
+// const makeImageCard = ({ url, alt }) => {
+//   const itemEl = document.createElement('li');
+
+//   const imageEl = document.createElement('img');
+//   imageEl.src = url;
+//   imageEl.alt = alt;
+
+//   itemEl.appendChild(imageEl);
+
+//   return itemEl;
+// };
+
+// const elements = images.map(makeImageCard);
+// console.log(elements);
+
+// imagesList.append(...elements)
+
+
+
+
+
