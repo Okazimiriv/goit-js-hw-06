3c7e98c6247a3c95cea2cdfafa917a99e44e4f89
Oksana Komarnytska <okazimiriv@gmail.com>
2023-03-17 18:06:13 +0100

    task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;  
+  const mail = formElements.email.value;
+  const password = formElements.password.value;  
+
+  if (mail === "" || password === "") {
+    alert("Всі поля повинні бути заповнені !!!");
+  }
+
+  console.log(`Login: ${mail}, Password: ${password}`);
+  event.currentTarget.reset();
+};
